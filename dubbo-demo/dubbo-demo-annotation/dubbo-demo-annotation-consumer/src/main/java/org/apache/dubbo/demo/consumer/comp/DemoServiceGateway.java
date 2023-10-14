991d55a7dd5df332cbc44ee3052e935c615ee2c5
chengtf94 <1404110179@qq.com>
2023-10-15 01:07:25 +0800

    add demo comment

--- dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-consumer/src/main/java/org/apache/dubbo/demo/consumer/comp/DemoServiceGateway.java
+++ dubbo-demo/dubbo-demo-annotation/dubbo-demo-annotation-consumer/src/main/java/org/apache/dubbo/demo/consumer/comp/DemoServiceGateway.java
@@ -23,18 +23,17 @@ import org.springframework.stereotype.Component;
 
 import java.util.concurrent.CompletableFuture;
 
-@Component("demoServiceComponent")
-public class DemoServiceComponent implements DemoService {
+@Component("demoServiceGateway")
+public class DemoServiceGateway {
     @DubboReference
     private DemoService demoService;
 
-    @Override
     public String sayHello(String name) {
         return demoService.sayHello(name);
     }
 
-    @Override
     public CompletableFuture<String> sayHelloAsync(String name) {
         return null;
     }
+
 }
